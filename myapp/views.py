8c24d756c5061d6714fb92d6ffa5b970a736beb5
Marcos da Silva Sampaio <marcos@sampaio.me>
2012-09-28 17:13:06 -0300

    Correção de form request

--- myapp/views.py
+++ myapp/views.py
@@ -3,25 +3,24 @@ from django.http import HttpResponseRedirect
 from myapp.forms import MyModelForm
 from myapp.models import MyModel
 
-
 def main(request):
     if request.method == 'POST':
         form = MyModelForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
-        return HttpResponseRedirect('/add') # Redirect after POST
+            request.session['name'] = name
+            mm = MyModel.objects.create(name=name)
+            mm.save()
+            return HttpResponseRedirect('/add') # Redirect after POST
     else:
         form = MyModelForm()
         args = {}
         args['form'] = form
     return render(request, 'main.html', args)
 
-
 def form_add(request):
     args = {}
     name = request.session['name']
     args['name'] = name
-    mm = MyModel(name=name)
-    mm.save()
 
     return render(request, 'add.html', args)
